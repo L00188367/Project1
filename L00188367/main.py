d8d02ca06255865f514fc45e3da9c836fc1b3505
Aseem <aseemkumargupta8712@gmail.com>
2024-11-21 19:20:25 +0000

    L00188367 started on Python Project1

---
+++ L00188367/main.py
@@ -0,0 +1,10 @@
+"""
+main.py
+By: L00188367
+Date: 21st Nov 2024
+There is a Source folder under the L00188367 
+"""
+
+from Source.directory_utilities import detect_os, detect_working_directory
+print(detect_os())
+print(detect_working_directory())     
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Project1
 Python Project
+
+Network Configuration is the project allocated to me. L00188367 
+
+
